4c9d7a88f9e9938ccea2408ea8b852860ea34b84
Anthony <anthony@aouka.com>
2016-11-26 21:36:08 +0100

    class system

---
+++ FormatML.php
@@ -0,0 +1,163 @@
+<?php
+namespace Coercive\Utility\Xml;
+
+use \Exception;
+
+/**
+ * FormatML
+ * PHP Version 	7
+ *
+ * @version		1
+ * @package 	Coercive\Utility\Xml
+ * @link		@link https://github.com/Coercive/Xml
+ *
+ * @author  	Anthony Moral <contact@coercive.fr>
+ * @copyright   (c) 2016 - 2017 Anthony Moral
+ * @license 	http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
+ */
+class FormatML {
+
+    const CONTENT = '_@$#&|||~~~REPLACE_CONTENT~~~|||&#$@_';
+
+    const OPTION_ALL_TAGS_REQUIRED = 'OPTION_ALL_TAGS_REQUIRED';
+    const OPTION_SKIP_MISSING_TAGS_CONTENT = 'OPTION_SKIP_MISSING_TAGS_CONTENT';
+
+    /** @var array */
+    private $_aSourceArray;
+
+    /** @var array */
+    private $_aFormatDirectives;
+
+    /** @var int */
+    private $_iAttributes = 0;
+
+    /** @var array */
+    private $_aAttributes = [];
+
+    /** @var string */
+    private $_sOutputString = '';
+
+    /**
+     * EXCEPTION
+     *
+     * @param string $sMessage
+     * @param int $sLine [optional]
+     * @param string $sMethod [optional]
+     * @throws Exception
+     */
+    static private function _exception($sMessage, $sLine = __LINE__, $sMethod = __METHOD__) {
+        throw new Exception("$sMessage \nMethod :  $sMethod \nLine : $sLine");
+    }
+
+    /**
+     * FormatML constructor.
+     *
+     * @param array $aArray
+     * @param array $aFormatDirectives [optional]
+     * @param array $aOptions [optional]
+     */
+    public function __construct($aArray, $aFormatDirectives = [], $aOptions = []) {
+
+        if(!$aArray || !is_array($aArray)) {
+            self::_exception("NOT A VALID SOURCE ARRAY OR EMPTY", __LINE__, __METHOD__);
+        }
+        $this->_aSourceArray = $aArray;
+
+        if(!is_array($aFormatDirectives)) {
+            self::_exception("NOT A VALID DIRECTIVE ARRAY", __LINE__, __METHOD__);
+        }
+        $this->_aFormatDirectives = $aFormatDirectives;
+
+        $this->_aOptions = array_replace_recursive([
+            self::OPTION_ALL_TAGS_REQUIRED => false,
+            self::OPTION_SKIP_MISSING_TAGS_CONTENT => false
+        ], $aOptions);
+
+        $this->_sOutputString = $this->_parse($this->_aSourceArray);
+    }
+
+    /**
+     * GET FORMATED STRING
+     *
+     * @return string
+     */
+    public function get() {
+        return $this->_sOutputString ?: '';
+    }
+
+    /**
+     * PARSE ARRAY TO STRING
+     *
+     * @param array $aArray
+     * @return string
+     */
+    private function _parse($aArray) {
+
+        # INIT
+        $sOutputString = '';
+
+        # EMPTY
+        if(empty($aArray)) { return ''; }
+
+        # PROCESSING
+        foreach($aArray as $mKey => $mItem) {
+
+            # ATTRIBUTES PREPARE
+            if($mKey === '@attributes') {
+                $this->_iAttributes++;
+                $this->_aAttributes[$this->_iAttributes] = $mItem;
+                continue;
+            }
+
+            # CONTENT
+            switch(gettype($mItem)) {
+                case 'string':
+                    $sOutputString .= htmlentities($mItem);
+                    break;
+                case 'boolean':
+                case 'integer':
+                case 'double':
+                case 'NULL':
+                    $sOutputString .= (string) $mItem;
+                    break;
+                case 'array':
+                    $sOutputString .= $this->_parse($mItem);
+                    break;
+                default:
+                    self::_exception('ITEM TYPE NO RECOGNIZE : ' . gettype($mItem), __LINE__, __METHOD__);
+            }
+
+            # CONTAINER
+            if(is_string($mKey) && isset($this->_aFormatDirectives[$mKey])) {
+
+                $mDirective = $this->_aFormatDirectives[$mKey];
+
+                switch(gettype($mDirective)) {
+
+                    case 'string':
+                        $sOutputString = str_replace(self::CONTENT, $sOutputString, $mDirective);
+                        break;
+
+                    case 'object':
+                        $sOutputString = $mDirective($sOutputString, $this->_iAttributes ? $this->_aAttributes[$this->_iAttributes] : []);
+                        break;
+
+                    default:
+                        self::_exception('NOT VALID DIRECTIVE : ' . $mKey, __LINE__, __METHOD__);
+
+                }
+            }
+            elseif (is_string($mKey) && $this->_aOptions[self::OPTION_ALL_TAGS_REQUIRED]) {
+                self::_exception('ALL TAGS REQUIRED, MISSING : ' . $mKey, __LINE__, __METHOD__);
+            }
+            elseif(is_string($mKey) && $this->_aOptions[self::OPTION_SKIP_MISSING_TAGS_CONTENT]) {
+                $sOutputString = '';
+            }
+
+        }
+
+        return html_entity_decode($sOutputString, ENT_QUOTES | ENT_HTML5);
+
+    }
+
+}
---
+++ XmlCleaner.php
@@ -0,0 +1,180 @@
+<?php
+namespace Coercive\Utility\Xml;
+
+use \Exception;
+
+/**
+ * XmlCleaner
+ * PHP Version 	7
+ *
+ * @version		1
+ * @package 	Coercive\Utility\Xml
+ * @link		@link https://github.com/Coercive/Xml
+ *
+ * @author  	Anthony Moral <contact@coercive.fr>
+ * @copyright   (c) 2016 - 2017 Anthony Moral
+ * @license 	http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
+ */
+class XmlCleaner {
+
+    # CLASS OPTIONS
+    const OPTION_DECODE = 'DECODE';
+    const OPTION_DELETE_DOCTYPE = 'DELETE_DOCTYPE';
+    const OPTION_DELETE_PARASITIC = 'DELETE_DELETE_PARASITIC';
+
+	/** @var array */
+	private $_aOptions;
+
+	/** @var string */
+	private $_sXML = '';
+
+	/**
+	 * EXCEPTION
+	 *
+	 * @param string $sMessage
+	 * @param int $sLine [optional]
+	 * @param string $sMethod [optional]
+	 * @throws Exception
+	 */
+	static private function _exception($sMessage, $sLine = __LINE__, $sMethod = __METHOD__) {
+		throw new Exception("$sMessage \nMethod :  $sMethod \nLine : $sLine");
+	}
+
+	/**
+	 * XMLCleaner constructor.
+	 *
+	 * @param array $aOptions [optional]
+	 */
+	public function __construct($aOptions = []) {
+
+		$this->_aOptions = array_replace_recursive([
+			self::OPTION_DECODE => [ ['&', '&amp;'] ],
+            self::OPTION_DELETE_DOCTYPE => true,
+            self::OPTION_DELETE_PARASITIC => true
+		], $aOptions);
+
+	}
+
+    /**
+     * GET XML CLEANED STRING
+     *
+     * @return string
+     */
+	public function get() {
+	    return $this->_sXML ?: '';
+    }
+
+	/**
+	 * FILE LOADER
+	 *
+	 * @param string $sXmlPath
+     * @return $this
+	 * @throws Exception
+	 */
+	public function loadFile($sXmlPath) {
+
+	    # SKIP ON ERROR
+        if(!file_exists($sXmlPath) || !is_file($sXmlPath)) {
+            self::_exception("NOT A VALID FILE : $sXmlPath", __LINE__, __METHOD__);
+        }
+
+		# SET
+		$this->_sXML = file_get_contents($sXmlPath);
+		if(!$this->_sXML) { self::_exception('FILE GET CONTENT ERROR OR EMPTY', __LINE__, __METHOD__); }
+
+		return $this;
+
+	}
+
+    /**
+     * FILE LOADER
+     *
+     * @param string $sXmlString
+     * @return $this
+     * @throws Exception
+     */
+    public function loadString($sXmlString) {
+
+        # SKIP ON ERROR
+        if(!$sXmlString || !is_string($sXmlString)) {
+            self::_exception("NOT A VALID STRING OR EMPTY", __LINE__, __METHOD__);
+        }
+
+        # SET
+        $this->_sXML = $sXmlString;
+
+        return $this;
+
+    }
+
+    /**
+     * CLEAN
+     *
+     * @return $this
+     */
+	public function clean() {
+
+        # DECODE
+        $this->_decode();
+
+        # DELETE DOCTYPE
+        $this->_deleteDoctype();
+
+        # DELETE PARASITIC
+        $this->_deleteParasitic();
+
+        return $this;
+
+    }
+
+	/**
+	 * DECODE GLOBAL & SPECIFIC
+     *
+     * @return void
+	 */
+	private function _decode() {
+
+		# HTML ENTITY
+		$this->_sXML = html_entity_decode($this->_sXML);
+
+		# DECODE SPECIFICS ATTRIBUTES
+		foreach($this->_aOptions[self::OPTION_DECODE] as $aSearchReplace) {
+			$this->_sXML = str_replace($aSearchReplace[0], $aSearchReplace[1], $this->_sXML);
+		}
+
+	}
+
+    /**
+     * DELETE DOCTYPE
+     *
+     * Delete goods and bads doctype witch can cause some xml-parsing errors
+     * Example : <!DOCTYPE myxml PUBLIC "-//MYXMLEXAMPLE//DTD MYXMLEXAMPLE XML//EN">
+     *
+     * @return void
+     */
+    private function _deleteDoctype() {
+
+        if($this->_aOptions[self::OPTION_DELETE_DOCTYPE]) {
+            $this->_sXML = preg_replace('`\<\!DOCTYPE [^<]*\>`i', '', $this->_sXML);
+        }
+
+    }
+
+    /**
+     * DELETE PARASITIC
+     *
+     * Delete parasitics elements witch cause fatal xml-parsing errors
+     * Examples :   <?lettrine;@0302?> ; <?tblwidth;126m?> ; <?bidencadre3;30;37m?> ; <?Pub Caret?>
+     *              <?Pub _bookmark Command="[Quick Mark]"?> ; <?xpp fin_fxapnot?> ; <?xpp error_tag #@@>
+     *
+     * @return void
+     */
+    private function _deleteParasitic() {
+
+        if($this->_aOptions[self::OPTION_DELETE_PARASITIC]) {
+            $this->_sXML = preg_replace('`\<\?(?!xml)[^<]*\>`i', '', $this->_sXML);
+        }
+
+    }
+
+}
